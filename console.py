6c114cca1ab12a96153efaa4e18dfdae64c33745
Abokiey <sweneysam@gmail.com>
2023-10-10 16:40:32 +0300

    consol

--- console.py
+++ console.py
@@ -5,8 +5,26 @@
 import cmd
 
 
-class HBNB(cmd.Cmd):
-    """defines the abnb interpreter"""
+class HBNBCommand(cmd.Cmd):
+    """defines the hbnb interpreter"""
     prompt = "(hbnb) "
 
+    def do_quit(self, line):
+        """quiting the shell"""
+        print("quitting the shell...")
+        return (True)
 
+    def do_EOF(self, line):
+        """end of file to exit"""
+        print("quitting the shell...")
+        return (True)
+
+    def emptyline(self):
+        """do nothing when an empty line"""
+        pass
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
