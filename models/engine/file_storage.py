d51f5463be66c7c5a61bb7ba1da8ec9c31b6ab5e
Abokiey <sweneysam@gmail.com>
2023-10-12 17:20:29 +0300

    consol

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+''' Module that serializes and deserializes '''
 
-"""serialization and deserialization of instances"""
-
-from models.base_model import BaseModel
 import os
 import json
-import models
+from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
@@ -15,30 +13,34 @@ from models.review import Review
 
 
 class FileStorage:
-    """serialize and deserialize"""
+    '''serialize and deserialize objects '''
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """return dictionary of objects"""
-        return (self.__objects)
+        """return dictionary of objects """
+        return (FileStorage.__objects)
 
     def new(self, obj):
         """sets objs with keys"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj.to_dict()
+        key = f'{obj.__class__.__name__}.{obj.id}'
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes objs to json files"""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(self.__objects, f)
+            json.dump(objdict, f)
 
     def reload(self):
         """deserializes json file to obj"""
         try:
-            with open(self.__file_path, "r") as f:
-                data = f.read()
-                if data:
-                    self.__objects = json.loads(data)
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
         except FileNotFoundError:
-            pass
+            return
