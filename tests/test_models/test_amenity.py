a52f5b5276d16033f2411ccd6edc37f6d8b68cda
Abokiey <sweneysam@gmail.com>
2023-10-13 21:34:28 +0300

    state

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""import modules"""
+
+
+import unittest
+from models.amenity import Amenity
+from models.base_model import BaseModel
+import models
+import datetime
+
+class TestAmenity(unittest.TestCase):
+    """instances of a method unittests"""
+    am = Amenity()
+
+    def test_class_exists(self):
+        """tests if class exists"""
+        result = "<class 'models.amenity.Amenity'>"
+        self.assertEqual(str(type(self.am)), result)
+
+    def test_types(self):
+        """tests if the type of the attribute is the correct one"""
+        self.assertIsInstance(self.am.name, str)
+        self.assertIsInstance(self.am.id, str)
+        self.assertIsInstance(self.am.created_at, datetime.datetime)
+        self.assertIsInstance(self.am.updated_at, datetime.datetime)
+
+    def testHasAttributes(self):
+        """verify if attributes exist"""
+        self.assertTrue(hasattr(self.am, 'name'))
+        self.assertTrue(hasattr(self.am, 'id'))
+        self.assertTrue(hasattr(self.am, 'created_at'))
+        self.assertTrue(hasattr(self.am, 'updated_at'))
